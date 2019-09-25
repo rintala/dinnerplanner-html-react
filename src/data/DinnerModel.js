8221533f3481c2b6f880f552e402f17cf894ef66
rintala <jrintala@kth.se>
2019-09-25 19:05:55 +0200

    Add global css stylesheet from prev labs, implemnet environment variable for secret API key, and implement our prev dinnerModel with some updates

--- src/data/DinnerModel.js
+++ src/data/DinnerModel.js
@@ -1,50 +1,204 @@
 import ObservableModel from "./ObservableModel";
-
-const BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com";
 const httpOptions = {
-  headers: { "X-Mashape-Key": "YOUR_API_KEY" }
+  method: "GET",
+  headers: {
+    "X-Mashape-Key": process.env.REACT_APP_SECRET_API_KEY
+  }
 };
 
-class DinnerModel extends ObservableModel {
+//DinnerModel class
+class DinnerModel {
   constructor() {
-    super();
-    this._numberOfGuests = 4;
-    this.getNumberOfGuests();
+    this.dishes = [];
+    this.GROUP_ID = 16;
+    this.baseURLRecipes =
+      "http://sunset.nada.kth.se:8080/iprog/group/" +
+      this.GROUP_ID +
+      "/recipes/";
+    this.spoonacularImagesURL = "https://spoonacular.com/recipeImages/";
+    //TODO Lab 0
+    // implement the data structure that will hold number of guests
+    // and selected dishes for the dinner menu
+    this.guests = 0;
+    this.menu = [];
+    this.currentDish = undefined;
+    this._observers = [];
   }
 
-  /**
-   * Get the number of guests
-   * @returns {number}
-   */
-  getNumberOfGuests() {
-    return this._numberOfGuests;
+  _handleHTTPError(response) {
+    if (response.ok) return response;
+    throw Error(response.statusText);
   }
 
-  /**
-   * Set number of guests
-   * @param {number} num
-   */
-  setNumberOfGuests(num) {
-    this._numberOfGuests = num;
-    this.notifyObservers();
-  }
-
-  // API methods
-
-  /**
-   * Do an API call to the search API endpoint.
-   * @returns {Promise<any>}
-   */
-  getAllDishes() {
-    const url = `${BASE_URL}/recipes/search`;
-    return fetch(url, httpOptions).then(this.processResponse);
-  }
-
-  processResponse(response) {
+  _handleHTTPErrorGetDish(response) {
     if (response.ok) {
-      return response.json();
+      return response.json().then(object => object);
+    } else {
+      return { code: response.status };
     }
-    throw response;
+  }
+
+  setNumberOfGuests(num) {
+    if (num > 0) this.guests = num;
+    this.updateObservers("input-num-guests");
+    this.updateObservers("value-num-guests");
+  }
+
+  getNumberOfGuests() {
+    return this.guests;
+  }
+
+  getSelectedDish(type) {
+    return new Promise((resolve, reject) => {
+      if (!type) {
+        resolve(this.getFullMenu());
+      }
+      //returns undefined if nothing is found
+      resolve(
+        this.menu.map(dish => {
+          if (Object.values(dish.dishTypes).indexOf(type) > -1) {
+            return dish;
+          }
+        })
+      );
+    });
+  }
+
+  getCurrentDish() {
+    return this.currentDish;
+  }
+
+  setCurrentDish(dish) {
+    this.currentDish = dish;
+  }
+
+  getDishPriceForNumberOfPeople(menuDish) {
+    return Math.round(menuDish.pricePerServing * this.getNumberOfGuests());
+  }
+
+  getFullMenu() {
+    return this.menu;
+  }
+
+  getAllIngredients() {
+    return Array.from(
+      new Set(
+        this.getFullMenu()
+          //using set to filter duplicates
+          .map(dish => dish.extendedIngredients)
+          .flat()
+      )
+    );
+  }
+
+  getTotalMenuPrice() {
+    if (this.menu.length === 0) {
+      return 0;
+    }
+    return this.menu
+      .map(dish => {
+        return dish.pricePerServing;
+      })
+      .reduce((sum, add) => sum + add)
+      .toFixed(2); //Rounding to 2 decimals
+  }
+
+  getTotalMenuPriceForNumberOfPeople() {
+    if (this.menu.length === 0) {
+      return 0;
+    }
+    return this.menu
+      .map(dish => {
+        return Math.round(dish.pricePerServing * this.getNumberOfGuests());
+      })
+      .reduce((sum, add) => sum + add)
+      .toFixed(2); //Rounding to 2 decimals
+  }
+
+  addDishToMenu(dishToAdd) {
+    //  console.log("adding dish to meni");
+    if (!this.menu.length) {
+      this.menu.push(dishToAdd);
+    } else {
+      this.menu = Array.from(
+        new Set([
+          ...this.menu.filter(dish => dish.id !== dishToAdd.id),
+          dishToAdd
+        ])
+      );
+    }
+  }
+
+  removeDishFromMenu(id) {
+    this.menu = this.menu.filter(dish => dish.id !== id);
+  }
+
+  //Returns all dishes of specific type (i.e. "starter", "main dish" or "dessert").
+  //query argument, text, if passed only returns dishes that contain the query in name or one of the ingredients.
+  //if you don't pass any query, all the dishes will be returned
+  getAllDishes(type, query) {
+    //TODO:
+
+    let url;
+    if (!type && !query) {
+      url = `http://sunset.nada.kth.se:8080/iprog/group/13/recipes/search?`;
+    } else {
+      url = `http://sunset.nada.kth.se:8080/iprog/group/13/recipes/search?query=${query}&dishTypes=${type}`;
+    }
+    return fetch(
+      url,
+
+      httpOptions
+    )
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return { code: res.status };
+        }
+      })
+      .then(data => data.results)
+      .then(data => console.log("data", data))
+      .catch(err => err);
+  }
+
+  //Returns a dish of specific ID
+  getDish(id) {
+    let url = `http://sunset.nada.kth.se:8080/iprog/group/13/recipes/${id}/information`;
+    return fetch(url, httpOptions)
+      .then(this._handleHTTPErrorGetDish)
+      .catch(console.error);
+  }
+
+  getFullDishImageURL(imageNameArray) {
+    if (imageNameArray && imageNameArray.length) {
+      return this.spoonacularImagesURL + imageNameArray[0];
+    }
+    return (
+      this.spoonacularImagesURL +
+      "matcha-green-tea-and-pineapple-smoothie-801710.jpg"
+    );
+  }
+  getDishImageURLFromString(imageNameString) {
+    if (imageNameString) {
+      return imageNameString;
+    }
+    return (
+      this.spoonacularImagesURL +
+      "matcha-green-tea-and-pineapple-smoothie-801710.jpg"
+    );
+  }
+
+  // observer functions
+  addObserver(observer) {
+    this._observers.push(observer);
+  }
+
+  // implemenent remove function for observers as well
+
+  updateObservers(detailsToUpdateWith) {
+    // instead define update function in each observer that doesnt re-render entire view
+    this._observers.forEach(obs => obs.updateView(detailsToUpdateWith));
   }
 }
 
